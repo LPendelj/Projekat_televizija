590fd5345f377624ea47f683423d4540cd73d71c
LPendelj <pendeljluka@gmail.com>
2022-02-25 12:49:58 +0100

    login method added

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,36 @@
 package main;
 
+import java.util.Scanner;
+
+import util.UserRepository;
+
 public class Main {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void login() {
+		System.out.println("Unesite korisnicko ime:");
+		
+		boolean korekt = false;
+		
+		while(!korekt) {
+		String user = sc.nextLine();
+		
+		System.out.println("Unesite sifru");
+		
+		String pass = sc.nextLine();
+		
+		
+		UserRepository repo = new UserRepository();
+		if(repo.getUserByUsername(user) == null) {
+			System.out.println("user or password are not correct");
+		} else {
+			if(repo.getUserByUsername(user).getPassword().equalsIgnoreCase(pass)) {
+				
+			};
+		}
+	}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/util/UserRepository.java
+++ src/util/UserRepository.java
@@ -1,11 +1,14 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.User;
 
 public class UserRepository implements Repository {
 
+	 List<User> lista;
+	
 	@Override
 	public void save(User user) {
 		// TODO Auto-generated method stub
@@ -21,6 +24,16 @@ public class UserRepository implements Repository {
 	@Override
 	public List<User> getAll() {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUserByUsername(String username) {
+		for(User u : lista) {
+			if(u.getUserName().equals(username)) {
+				return u;
+			}
+		}
+		
 		return null;
 	}
 
